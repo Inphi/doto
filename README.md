79284c30a5447002a1c92fe7bc11ea12d311e622
inphi <mlaw2501@gmail.com>
2023-10-03 10:12:59 -0400

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Defense of The Outputs
+
+Join the battle against illegitimate disputes by defending proposed outputs on Optimism Goerli. This repository is your gateway to an effortless participation in the Fault Dispute Game (FDG).
+
+## Prequisites
+
+Make sure you have the following installed on your system:
+- docker (with docker compose)
+- [foundry](https://book.getfoundry.sh/getting-started/installation)
+- At least 30 GB of disk
+
+## Configuration:
+
+Create a .env file with the following envars set:
+- OP_CHALLENGER_L1_ETH_RPC - A Goerli Goerli ETH-RPC endpoint 
+- OP_CHALLENGER_CANNON_L2 - A Optimism Goerli ETH-RPC endpoint
+- OP_CHALLENGER_PRIVATE_KEY - Your Goerli account private key
+
+## Running the Defender
+
+The Defender is a [op-challenger](https://github.com/ethereum-optimism/optimism/tree/develop/op-challenger) preconfigured to participate on Fault Dispute Games created by the
+Fault Dispute Game Factory contract on Goerli.
+Once the above configs are set, simply run `bash defend.sh` to begin. The defend script will run the op-challenger on a single random fault dispute game.
+If you'd like to participate in multiple games then you should join the frontlines.
+
+## Fronline Defense
+
+If you have sufficient disk (and CPU), you can participate in defending outputs from the frontline by participating in _all_ disputes games.
+Simply run `docker compose up` to join the frontlines.
+
+Once you have the above configs setup, you can simply run `docker compose up`.
---
+++ defend.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .env
+
+RPC_URL=$OP_CHALLENGER_L1_ETH_RPC
+GAME_FACTORY_ADDRESS=0xad9e5E6b39F55EE7220A3dC21a640B089196a89f
+
+COUNT=$(cast call --rpc-url $RPC_URL $GAME_FACTORY_ADDRESS 'gameCount() returns(uint256)')
+((COUNT=COUNT-1))
+GAME_INDICES=()
+for i in $(seq 0 "${COUNT}")
+do
+  GAME=$(cast call --rpc-url "${RPC_URL}" "${GAME_FACTORY_ADDRESS}" 'gameAtIndex(uint256) returns(uint8, uint64, address)' "${i}")
+
+  SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
+  IFS=$'\n'      # Change IFS to newline char
+  GAME=($GAME) # split the string into an array by the same name
+  IFS=$SAVEIFS   # Restore original IFS
+
+  GAME_ADDR="${GAME[2]}"
+  CLAIMS=$(cast call --rpc-url "${RPC_URL}" "${GAME_ADDR}" "claimDataLen() returns(uint256)")
+  STATUS=$(cast call --rpc-url "${RPC_URL}" "${GAME_ADDR}" "status() return(uint8)" | cast to-dec)
+  if [[ "${STATUS}" == "0" ]]
+  then
+      echo "Found in-progress game at ${i} index"
+      GAME_INDICES+=("${i}")
+  fi
+done
+
+COUNT_INPROGRESS=${#GAME_INDICES[@]}
+if [ "$COUNT_INPROGRESS" -eq 0 ]; then
+    echo "There are no games in-progress. Exiting"
+    exit 0
+fi
+
+INDEX=$((RANDOM % COUNT_INPROGRESS))
+GAME_INDEX=${GAME_INDICES[$INDEX]}
+GAME_ADDR=$(cast call --rpc-url "${RPC_URL}" "${GAME_FACTORY_ADDRESS}" 'gameAtIndex(uint256) returns(uint8, uint64, address)' "${GAME_INDEX}" | awk 'NR==3')
+STATUS=$(cast call --rpc-url "${RPC_URL}" "${GAME_ADDR}" "status() return(uint8)" | cast to-dec)
+
+export OP_CHALLENGER_GAME_ALLOWLIST="${GAME_ADDR}"
+echo "Starting op-challenger for ${OP_CHALLENGER_GAME_ALLOWLIST}"
+docker compose up op-challenger-whitelisted
